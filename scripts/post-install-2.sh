8ab24df3eb9bb1c38dacb313733a24ead634c14e
Dawid Imb <dawid.imb2010@gmail.com>
2025-01-12 20:03:16 +0100

    added jellyfin compose.yaml added post install scripts

---
+++ scripts/post-install-2.sh
@@ -0,0 +1,12 @@
+
+curl -O https://raw.githubusercontent.com/DawidDe/Proxmox-Scripts/refs/heads/master/apps/ct/nginx-proxy-manager.sh
+bash nginx-proxy-manager.sh
+rm nginx-proxy-manager.sh
+
+curl -O https://raw.githubusercontent.com/DawidDe/Proxmox-Scripts/refs/heads/master/apps/ct/vaultwarden.sh
+bash vaultwarden.sh
+rm vaultwarden.sh
+
+curl -O https://raw.githubusercontent.com/DawidDe/Proxmox-Scripts/refs/heads/master/apps/ct/jellyfin.sh
+bash jellyfin.sh
+rm jellyfin.sh
