0ac047c9e285a357e8731ba5daabd30b174424c5
Dawid Imb <dawid.imb2010@gmail.com>
2025-01-12 18:11:56 +0100

    worked on install.sh

--- apps/ct/nginx-proxy-manager.sh
+++ apps/ct/nginx-proxy-manager.sh
@@ -2,3 +2,22 @@
 
 # Some important variables
 TYPE="docker"
+APP="nginx-proxy-manager"
+CTID="100"
+TEMPLATE="alpine-3.21-default_20241217_amd64.tar.xz"
+CPU_CORES="1"
+RAM_SIZE="1024"
+DISK_SIZE="10"
+IP="192.168.178.4/24"
+GATEWAY="192.168.178.1"
+ROOT_PASSWORD="dawid"
+HOSTNAME="npm"
+UNPRIVILEGED=""
+NESTED=""
+
+prepare_os_docker
+
+create_lxc_container
+
+# Create a new container
+create_lxc_container
--- scripts/install.sh
+++ scripts/install.sh
@@ -10,3 +10,19 @@ prepare_os() {
     apk update
     apk upgrade
 }
+
+create_lxc_container() {
+    pct create $CTID $TEMPLATE \
+    --cores $CPU_CORES \
+    --memory $RAM_SIZE \
+    --rootfs local-lvm:$DISK_SIZE \
+    --net0 name=eth0,bridge=vmbr0,firewall=1,gw=$GATEWAY,ip=$IP \
+    --password $ROOT_PASSWORD \
+    --hostname $HOSTNAME \
+    --unprivileged $UNPRIVILEGED \
+    --features nesting=$NESTED
+}
+
+docker_preparation() {
+    
+}
